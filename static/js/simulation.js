fe93ffd1ee473f1fe99a638622378e337f42f315
abrothguer <abrothguer@gmail.com>
2019-06-26 00:37:03 -0300

    Finished tab implementation for user view

--- static/js/simulation.js
+++ static/js/simulation.js
@@ -1,6 +1,12 @@
 // Variáveis Globais
 var currentClient = 0;
+var currentSimulation = 0;
+
 var intervalId = null;
+var simuInterval = null;
+
+var simulationCount = $("#simulation-count").val();
+simulationCount = 20;
 
 // Objetos para plotar os gráficos
 var traceArrivals = {
@@ -147,14 +153,126 @@ function fetchAndFill(){
     })
 }
 
+var tableTemplate = '\
+    <table class="table table-bordered">\
+        <thead class="thead-dark">\
+            <tr>\
+                <th>Cliente</th>\
+                <th>Tempo desde a última chegada</th>\
+                <th>Tempo de chegada</th>\
+                <th>Tempo de serviço</th>\
+                <th>Tempo de início do serviço</th>\
+                <th>Tempo de fila</th>\
+                <th>Tempo final do serviço</th>\
+                <th>Tempo gasto no sistema</th>\
+                <th>Tempo livre do operador</th>\
+            </tr>\
+        </thead>\
+        <tbody id="tbody-{{simuNumber}}">\
+        </tbody>\
+    </table>'
+
+var navTab = '<a class="nav-item nav-link active" data-toggle="tab" href="#tab-{{simuNumber}}" role="tab" aria-controls="public" aria-expanded="true">#{{simuNumber}} Sim.</a>'
+var divTab = '<div role="tabpanel" class="tab-pane fade active show mt-2" id="tab-{{simuNumber}}" aria-labelledby="public-tab" aria-expanded="true"></div>'
+
+var tableRow = document.createElement("TR");
+for(var j = 0; j < 9; ++j){
+    tableRow.appendChild(document.createElement("TD"));
+}
+
+var tabLink = document.createElement("A");
+tabLink.setAttribute("class", "nav-item nav-link");
+tabLink.setAttribute("data-toggle", "tab");
+tabLink.setAttribute("role", "tab");
+
+var tabDiv = document.createElement("DIV");
+tabDiv.setAttribute("role", "tabpanel");
+tabDiv.setAttribute("class", "tab-pane fade mt-2");
+tabDiv.setAttribute("aria-expanded", "false");
+
+function fillRow(simuNumber, rowNumber, row){
+    columns = document.getElementById("sim-" + simuNumber + "-clt-" + rowNumber).children;
+    columns[1].textContent = row["arrival-last"];
+    columns[2].textContent = row["arrival-total"];
+    columns[3].textContent = row["attendance"];
+    columns[4].textContent = row["attendance-begin"];
+    columns[5].textContent = row["queue"];
+    columns[6].textContent = row["attendance-end"];
+    columns[7].textContent = row["system-total"];
+    columns[8].textContent = row["server-free"];
+}
+
+function renderTable(simuNumber, nRows, simulation){
+
+    linkClone = tabLink.cloneNode(true);
+    linkClone.setAttribute("href", "#tab-" + simuNumber);
+    linkClone.innerHTML = "#" + simuNumber + " Sim."
+    document.getElementById("slider-tab").appendChild(linkClone);
+
+    divClone = tabDiv.cloneNode(true);
+    divClone.setAttribute("id", "tab-" + simuNumber);
+    document.getElementById("tab-content").appendChild(divClone);
+
+    document.getElementById("tab-" + simuNumber).innerHTML = tableTemplate.replace("{{simuNumber}}", simuNumber);
+    var tbody = document.getElementById("tbody-" + simuNumber);
+
+    for(var i = 1; i <= nRows; ++i){
+        rowClone = tableRow.cloneNode(true);
+        rowClone.setAttribute("id", "sim-" + simuNumber + "-clt-" + i);
+        rowClone.children[0].setAttribute("class", "font-weight-bold");
+        rowClone.children[0].innerHTML = i;
+
+        tbody.appendChild(rowClone);
+        fillRow(simuNumber, i, simulation[i-1]);
+    }
+    reAdjust();
+
+    /*
+    for(var i = 0; i < nRows; ++i){
+        var row = document.createElement("TR");
+        row.setAttribute("id", "sim-" + simuNumber + "-clt-" + i);
+
+        var client = document.createElement("TD");
+        client.setAttribute("class", "font-weight-bold");
+        client.innerHTML = i;
+        row.appendChild(client);
+
+        for(var j = 0; j < 8; ++j){
+            row.appendChild(document.createElement("TD"));
+        }
+        tbody.appendChild(row);
+    }
+    */
+}
+
+function fetchSimulation(){
+    if(currentSimulation >= simulationCount){
+        stopSimulation();
+        return;
+    }
+    ++currentSimulation;
+
+    fetch(simulationURL)
+    .then(response => {
+        response.text().then(rawData => {
+
+            var serverResponse = JSON.parse(rawData);
+            console.log("RESPONSE", serverResponse);
+            renderTable(currentSimulation, simulationCount, serverResponse)
+        })
+    })
+}
+
 function startSimulation(){
     var simGap = parseFloat(document.getElementById("sim-gap").value);
     simGap = isNaN(simGap) || simGap <= 0 ? 1000 : simGap*1000;
     intervalId = setInterval(fetchAndFill, simGap);
+    simuInterval = setInterval(fetchSimulation, simGap);
 }
 
 function stopSimulation(){
     window.clearInterval(intervalId);
+    window.clearInterval(simuInterval);
 }
 
 
@@ -171,12 +289,12 @@ var hidWidth;
 var scrollBarWidths = 40;
 
 var widthOfList = function(){
-  var itemsWidth = 0;
-  $('.list a').each(function(){
-    var itemWidth = $(this).outerWidth();
-    itemsWidth+=itemWidth;
-  });
-  return itemsWidth;
+    var itemsWidth = 0;
+    $('.list a').each(function(){
+        var itemWidth = $(this).outerWidth();
+        itemsWidth+=itemWidth;
+    });
+    return itemsWidth;
 };
 
 var widthOfHidden = function(){
@@ -195,7 +313,6 @@ var widthOfHidden = function(){
 };
 
 var getLeftPosi = function(){
-
     var ww = 0 - $('.wrapper').outerWidth();
     var lp = $('.list').position().left;
 
@@ -238,7 +355,9 @@ $('.scroller-right').click(function() {
   $('.scroller-left').fadeIn('slow');
   $('.scroller-right').fadeOut('slow');
 
-  $('.list').animate({left:"+="+widthOfHidden()+"px"},'slow',function(){
+  var pace = widthOfHidden();
+  pace = pace > -500 ? pace-100 : -500;
+  $('.list').animate({left:"+="+pace+"px"},'slow',function(){
     reAdjust();
   });
 });
@@ -248,57 +367,8 @@ $('.scroller-left').click(function() {
 	$('.scroller-right').fadeIn('slow');
 	$('.scroller-left').fadeOut('slow');
 
+    console.log(getLeftPosi());
   	$('.list').animate({left:"-="+getLeftPosi()+"px"},'slow',function(){
   	    reAdjust();
   	});
 });
-
-
-var myBigTemplate = '<section id="simu-table" class="mt-5"> \
-    <hr>\
-    <h3 class="mb-3"> Tabela de Simulação</h3>\
-    <table class="table table-bordered">\
-        <thead class="thead-dark">\
-            <tr>\
-                <th>Cliente</th>\
-                <th>Tempo desde a última chegada</th>\
-                <th>Tempo de chegada</th>\
-                <th>Tempo de serviço</th>\
-                <th>Tempo de início do serviço</th>\
-                <th>Tempo de fila</th>\
-                <th>Tempo final do serviço</th>\
-                <th>Tempo gasto no sistema</th>\
-                <th>Tempo livre do operador</th>\
-            </tr>\
-        </thead>\
-        <tbody>\
-            {% for clt in range(simulation.clients) %}\
-            <tr id="client_{{ clt + 1}}">\
-                <td class="font-weight-bold" scope="row">{{ clt + 1}}</td>\
-                <td>-</td>\
-                <td>-</td>\
-                <td>-</td>\
-                <td>-</td>\
-                <td>-</td>\
-                <td>-</td>\
-                <td>-</td>\
-                <td>-</td>\
-            </tr>\
-            {% endfor %}\
-        </tbody>\
-    </table>\
-</section>\
-<section id="simu-graphs" class="mt-5">\
-    <hr class="w-100">\
-    <h3>Gráficos da Simulação</h3>\
-    <div class="row">\
-        <div class="col-sm-12">\
-            <div id="arrival-attendance-graph">\
-            </div>\
-        </div>\
-        <div id="queue-graph" class="col-sm-6">\
-        </div>\
-        <div id="system-graph" class="col-sm-6">\
-        </div>\
-    </div>\
-</section>';
